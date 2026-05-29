1ee09dd376e29a3924ded4cea49b91d3c2acc1c4
stbtdiet <bruno.xylophon@gmail.com>
2026-05-29 19:55:42 +0200

    Revert state to commit e9930

--- src/sink/stream_sink.rs
+++ src/sink/stream_sink.rs
@@ -61,15 +61,7 @@ impl<W: Write + Send> Sink for StreamSink<W> {
         info.write_to(&mut self.writer)?;
 
         // Write Tracks element inside the segment
-        match ebml_header.doc_type {
-            Some(DocType(ref doc_type)) if doc_type.to_lowercase() == ContainerFormat::WebM.to_string() => {
-                let patched_tracks = crate::sink::util::webm_patch::patch_tracks_for_webm(tracks)?;
-                self.writer.write_all(&patched_tracks)?;
-            }
-            _ => {
-                tracks.write_to(&mut self.writer)?;
-            }
-        }
+        tracks.write_to(&mut self.writer)?;
 
         // Write Chapters element inside the segment (if present)
         if let Some(chapters) = chapters {
--- src/sink/util/mod.rs
+++ src/sink/util/mod.rs
@@ -1,4 +1,3 @@
 pub mod output_sink;
-pub mod webm_patch;
 
 pub use output_sink::{OutputSink, Uninitialized, Initialized};
